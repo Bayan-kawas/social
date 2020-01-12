4222b4de3a614ec36ed0d1de223f9b1ea8ed0cb3
Bayan-kawas <bayan.kawas99@gmail.com>
2020-01-12 17:48:33 +0200

    pushed

---
+++ lib/creatDBbdart.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:mock_data/mock_data.dart';
+import 'package:collection/collection.dart';
+String names = mockName();
+String posts = mockString(100);
+int i;
+
+Future createDb() async {
+  bool dataSeeded = false;
+  if (dataSeeded == false) {
+    // get a location
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath,
+        'demo.db'); //join is not defined without import path package
+    // open database
+    Database database = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(
+          'CREAT TABLE users(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)');
+      await db.execute(
+          'CREAT TABLE posts(id INTEGER PRIMARY KEY, content TEXT, user_id INTEGER)');
+      await db.execute(
+          'CREAT TABLE comments(id INTEGER PRIMARY KEY, content TEXT, user_id INTEGER, post_id INTEGER)');
+      await db.execute(
+          'CREAT TABLE post_likes(id INTEGER PRIMARY KEY, user_id INTEGER, post_id INTEGER)');
+      await db.execute(
+          'CREAT TABLE comment_likes(id INTEGER PRIMARY KEY, user_id INTEGER, comment_id INTEGER)');
+    });
+    // insert some record
+    insertData(database);
+    readDatabase(database);
+  }
+}
+
+Future insertData(Database database) async {
+  for (i = 0; i < 10; i++) {
+    await database.transaction((txn) async {
+      await txn.rawInsert(
+          'INSERT INTO users(first_name, last_name) VALUES($names, $names)');
+    });
+  }
+}
+Future readDatabase(Database database)async{
+  List<Map> list = await database.rawQuery('SELECT * FROM users');
+  List<Map> expectedList = [
+  {'id':'id', 'first_name': names, 'last_name':names},
+  {'id':'id', 'first_name': names, 'last_name': names}
+  ];
+  print(list);
+  print(expectedList);
+  assert(const DeepCollectionEquality().equals(list, expectedList));//SECOND PROBLEM
+
+// Count the records
+ int count = Sqflite
+      .firstIntValue(await database.rawQuery('SELECT COUNT(*) FROM users'));
+  assert(count == 2);
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'creatDBbdart.dart';
+void main()async {
+  createDb();
+}
+
+
+
+
+
+//CREATE TABLE `bayan`.`users` ( `id` INT NOT NULL , `first name` TEXT NOT NULL , `last name` TEXT NOT NULL ) ENGINE = InnoDB;
+//CREATE TABLE `bayan`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `first name` TEXT NOT NULL , `last name` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+//CREATE TABLE `bayan`. ( `id` INT NOT NULL , `content` TEXT NOT NULL , `user_id` INT NOT NULL , `post_id` INT NOT NULL ) ENGINE = InnoDB;
+//
+//CREATE TABLE `bayan`.`comment likes` ( `id` INT NOT NULL , `user_id` INT NOT NULL , `comment_id` INT NOT NULL ) ENGINE = InnoDB;
+
